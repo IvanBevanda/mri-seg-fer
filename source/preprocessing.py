a0aabf0199a6c152b31c07bbcb6f4267b40619a7
Ivan Bevanda <ib55199@fer.hr>
2026-01-17 14:05:45 +0100

    Added logging during training. Black formatting.

--- source/preprocessing.py
+++ source/preprocessing.py
@@ -6,8 +6,7 @@ from tqdm import tqdm
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(
-    BASE_DIR,
-    "..", "data", "archive", "brisc2025", "segmentation_task"
+    BASE_DIR, "..", "data", "archive", "brisc2025", "segmentation_task"
 )
 
 DATA_DIR = os.path.abspath(DATA_DIR)
@@ -20,17 +19,18 @@ OUTPUT_DIR = os.path.join(ROOT_DIR, "output")
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 print("Output folder:", OUTPUT_DIR)
 
-IMG_SIZE  = (256, 256)
-CHANNELS  = 1
+IMG_SIZE = (256, 256)
+CHANNELS = 1
+
 
 def load_images_and_masks(split):
     images_dir = os.path.join(DATA_DIR, split, "images")
-    masks_dir  = os.path.join(DATA_DIR, split, "masks")
+    masks_dir = os.path.join(DATA_DIR, split, "masks")
 
     X, Y = [], []
 
     for fname in tqdm(os.listdir(images_dir)):
-        img_path  = os.path.join(images_dir, fname)
+        img_path = os.path.join(images_dir, fname)
         mask_name = fname.replace(".jpg", ".png")
         mask_path = os.path.join(masks_dir, mask_name)
 
@@ -38,18 +38,18 @@ def load_images_and_masks(split):
             print("Missing mask for:", fname)
             continue
 
-        img  = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
         mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
 
-        img  = cv2.resize(img,  IMG_SIZE)
+        img = cv2.resize(img, IMG_SIZE)
         mask = cv2.resize(mask, IMG_SIZE)
 
-        img  = img.astype(np.float32) / 255.0
+        img = img.astype(np.float32) / 255.0
 
-        mask  = (mask > 0).astype(np.uint8)
+        mask = (mask > 0).astype(np.uint8)
 
-        img   = np.expand_dims(img,  axis=0)
-        mask  = np.expand_dims(mask, axis=0)
+        img = np.expand_dims(img, axis=0)
+        mask = np.expand_dims(mask, axis=0)
 
         X.append(img)
         Y.append(mask)
@@ -57,6 +57,7 @@ def load_images_and_masks(split):
     X = np.stack(X)
     Y = np.stack(Y)
     return X, Y
+
 
 train_X, train_Y = load_images_and_masks("train")
 np.save(os.path.join(OUTPUT_DIR, "train_X.npy"), train_X)
@@ -67,7 +68,7 @@ test_X, test_Y = load_images_and_masks("test")
 np.save(os.path.join(OUTPUT_DIR, "test_X.npy"), test_X)
 np.save(os.path.join(OUTPUT_DIR, "test_Y.npy"), test_Y)
 
-print("train_X:",  train_X.shape)
-print("train_Y:",  train_Y.shape)
-print("test_X:",   test_X.shape)
-print("test_Y:",   test_Y.shape)
+print("train_X:", train_X.shape)
+print("train_Y:", train_Y.shape)
+print("test_X:", test_X.shape)
+print("test_Y:", test_Y.shape)
